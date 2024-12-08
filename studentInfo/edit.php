338adfe77a7acce0582d12d8ab6e2d800144378c
bornohin2019 <bornohin2019@gmail.com>
2024-12-08 12:55:23 +0600

    insert

---
+++ studentInfo/edit.php
@@ -0,0 +1,48 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "students_info";
+
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['update'])) {
+    $id = $_POST['id'];
+    $newName = $_POST['name'];
+    $newEmail = $_POST['email'];
+
+    $sql = "UPDATE student_info SET name = '$newName', email = '$newEmail' WHERE id = $id";
+
+    if ($conn->query($sql)) {
+
+        header("Location: showDb_delet_edit.php");  
+    //     exit(); 
+    // } else {
+    //     echo "Error: " . $conn->error;
+    // }
+}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Student</title>
+</head>
+<body>
+    <h1>Update Student</h1>
+    <form method="POST">
+        <input type="number" name="id" placeholder="ID" required><br>
+        <input type="text" name="name" placeholder="New Name" required><br>
+        <input type="email" name="email" placeholder="New Email" required><br>
+        <button type="submit" name="update">Update</button>
+    </form>
+</body>
+</html>
+
+<?php $conn->close(); ?>
---
+++ studentInfo/edit2.php
@@ -0,0 +1,4 @@
+<?php
+$dbb_connect
+
+?>
